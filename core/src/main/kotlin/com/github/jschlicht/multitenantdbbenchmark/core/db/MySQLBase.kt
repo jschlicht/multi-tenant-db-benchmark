36218dc2537bdaee1f4299dec59f2afb0a38040c
Jared Schlicht <jschlicht@gmail.com>
2024-01-24 15:32:40 -0800

    Revert todo comment, partitioning is now supported by mysql/mariadb

--- core/src/main/kotlin/com/github/jschlicht/multitenantdbbenchmark/core/db/MySQLBase.kt
+++ core/src/main/kotlin/com/github/jschlicht/multitenantdbbenchmark/core/db/MySQLBase.kt
@@ -6,20 +6,6 @@ import org.jooq.SQLDialect
 abstract class MySQLBase(key: String, dialect: SQLDialect, defaultSchema: String) :
     Database(key, dialect, defaultSchema) {
 
-    override fun supports(strategy: Strategy): Boolean {
-        return when (strategy) {
-            Namespace,
-            Normalized,
-            TenantIdComposite,
-            TenantIdSimple -> true
-
-            DistributedTable -> false // citus only
-
-            PartitionHash,
-            PartitionList -> true // TODO: Remove comment: technically possible, but we lose auto-incrementing keys and foreign keys.
-        }
-    }
-
     override fun supportsForeignKeysWith(strategy: Strategy): Boolean {
         return when (strategy) {
             PartitionHash,
