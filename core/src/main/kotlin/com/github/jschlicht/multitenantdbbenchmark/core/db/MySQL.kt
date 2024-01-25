0e29eb05a07cde36214cb6c53dbe29cff9439e45
Jared Schlicht <jschlicht@gmail.com>
2024-01-24 18:45:32 -0800

    Increase mysql connect timeout for github actions.

--- core/src/main/kotlin/com/github/jschlicht/multitenantdbbenchmark/core/db/MySQL.kt
+++ core/src/main/kotlin/com/github/jschlicht/multitenantdbbenchmark/core/db/MySQL.kt
@@ -2,11 +2,16 @@ package com.github.jschlicht.multitenantdbbenchmark.core.db
 
 import org.jooq.SQLDialect
 import org.testcontainers.containers.MySQLContainer
+import java.time.Duration
 
 data object MySQL : MySQLBase("mysql", SQLDialect.MYSQL, "main") {
     override fun createContainer(): MySQLContainer<*> {
         return MySQLContainer("mysql")
             .withDatabaseName(defaultSchema)
+            .withConnectTimeoutSeconds(CONNECT_TIMEOUT_SECONDS)
             .withUrlParam("serverTimezone", "Etc/UTC")
     }
+
+    // MySQL can be slow to start up on github actions, so give it more time.
+    private const val CONNECT_TIMEOUT_SECONDS = 60*5
 }
